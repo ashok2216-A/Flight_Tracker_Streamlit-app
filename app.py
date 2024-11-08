4405da6447ccb8048966a514a2befcdcac826677
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-11-08 19:56:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ with st.sidebar:
 flight_tracking(flight_view_level=view, country=cou,flight_info=info,
             local_time_zone=time, airport=air_port, color=clr)
 st.subheader('Ask your Questions!', divider='rainbow')
-st.write("Google's TAPAS base model fine-tuned on WikiTable Questions (WTQ)")
+st.write("Google's TAPAS base model")
 geo_df = flight_data(flight_view_level = view, country= cou, flight_info=info, local_time_zone=time, airport=1)
 question = st.text_input('Ask your question', "What is the squawk code for SWR9XD?")
 result = query_flight_data(geo_df, question)
